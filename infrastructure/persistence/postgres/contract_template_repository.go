d3ee4afb6c11519778278f64c22ee994849c6581
AzizRahimov <aziz.rahimov0001@gmail.com>
2022-07-10 17:00:04 +0500

    update

--- infrastructure/persistence/postgres/contract_template_repository.go
+++ infrastructure/persistence/postgres/contract_template_repository.go
@@ -82,7 +82,8 @@ func (r *ContractTemplateRepository) GetAllContracts(ctx context.Context, contra
 	var brands2 model.Brand
 
 	var contractStatusRus = ""
-	sqlQuery := "SELECT * FROM contracts WHERE id not in (select prev_contract_id from contracts) AND is_active = true"
+	//sqlQuery := "SELECT * FROM contracts WHERE id not in (select prev_contract_id from contracts) AND is_active = true"
+	sqlQuery := "SELECT id FROM contracts"
 	sqlQueryBrand := "SELECT id, brand, brand_code, discount_percent FROM brands WHERE id = 31"
 	sqlQueryBrands := "SELECT id, brand, brand_code, discount_percent FROM brands"
 
@@ -124,7 +125,8 @@ func (r *ContractTemplateRepository) GetAllContracts(ctx context.Context, contra
 	items := make([]model.ContractWithJsonB, 0)
 	for rows.Next() {
 		i := model.ContractWithJsonB{}
-		rows.Scan(&i.ID, &i.PrevContractId, &i.Status, &i.Requisites, &i.Manager, &i.Type, &i.SupplierCompanyManager, &i.ContractParameters, &i.Products, &i.Discounts, &i.Comment, &i.KAM, &i.UpdatedAt, &i.CreatedAt, &i.WithTemperatureConditions, &i.IsIndivid, &i.ExtContractCode)
+		//rows.Scan(&i.ID, &i.PrevContractId, &i.Status, &i.Requisites, &i.Manager, &i.Type, &i.SupplierCompanyManager, &i.ContractParameters, &i.Products, &i.Discounts, &i.Comment, &i.KAM, &i.UpdatedAt, &i.CreatedAt, &i.WithTemperatureConditions, &i.IsIndivid, &i.ExtContractCode)
+		rows.Scan(&i.ID)
 		fmt.Println("внутри цикла ", i)
 		items = append(items, i)
 
