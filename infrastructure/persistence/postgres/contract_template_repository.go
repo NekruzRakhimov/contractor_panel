695d2137ad04001af0bee6dfb55c0e972397c36a
AzizRahimov <aziz.rahimov0001@gmail.com>
2022-07-11 19:52:16 +0500

    fix #2

--- infrastructure/persistence/postgres/contract_template_repository.go
+++ infrastructure/persistence/postgres/contract_template_repository.go
@@ -85,7 +85,7 @@ func (r *ContractTemplateRepository) GetAllContracts(ctx context.Context, contra
 	var contractStatusRus = ""
 	//sqlQuery := "SELECT * FROM contracts WHERE id not in (select prev_contract_id from contracts) AND is_active = true"
 
-	sqlQuery := "SELECT id FROM contracts"
+	sqlQuery := "SELECT id, contract_parameters FROM contracts"
 
 	//sqlQuery := "SELECT  id, type, status, requisites, manager,  contract_parameters," +
 	//	" created_at, updated_at, is_individ, additional_agreement_number, ext_contract_code FROM contracts"
@@ -133,7 +133,7 @@ func (r *ContractTemplateRepository) GetAllContracts(ctx context.Context, contra
 		i := model.ContractWithJsonB{}
 
 		//rows.Scan(&i.ID, &i.PrevContractId, &i.Status, &i.Requisites, &i.Manager, &i.Type, &i.SupplierCompanyManager, &i.ContractParameters, &i.Products, &i.Discounts, &i.Comment, &i.KAM, &i.UpdatedAt, &i.CreatedAt, &i.WithTemperatureConditions, &i.IsIndivid, &i.ExtContractCode)
-		err := rows.Scan(&i.ID)
+		err := rows.Scan(&i.ID, &i.ContractParameters)
 		//err := rows.Scan(&i.ID, &i.Type, &i.Status, &i.Requisites, &i.Manager, &i.ContractParameters, &i.CreatedAt, &i.UpdatedAt, &i.IsIndivid, &i.AdditionalAgreementNumber, &i.ExtContractCode)
 		if err != nil {
 			fmt.Println("ERROR", err)
--- main.go
+++ main.go
@@ -2,28 +2,27 @@ package main
 
 import (
 	"contractor_panel/cmd"
-	"os"
 )
 
 func main() {
-	os.Setenv("PORT", "8080")
-	os.Setenv("DATASOURCES_POSTGRES_HOST", "ec2-18-235-154-252.compute-1.amazonaws.com")
-	os.Setenv("HEALTHCHECK_TIMEOUT", "30s")
-	os.Setenv("DATASOURCES_POSTGRES_USER", "obktylemltwdzc")
-	os.Setenv("DATASOURCES_POSTGRES_PASSWORD", "3492ecaa68258e934a6b71d0fa502f99d45ce6c159a588a403dbdd789aeedf84")
-	os.Setenv("DATASOURCES_POSTGRES_DATABASE", "ddgndiu11pio51")
-	os.Setenv("DATASOURCES_POSTGRES_PORT", "5432")
-	os.Setenv("DATASOURCES_POSTGRES_SCHEMA", "public")
-	os.Setenv("APP_NAME", "contractor-panel")
-	os.Setenv("APP_INSTANCE", "instance1")
-	os.Setenv("LOG_PRETTY_PRINT", "1")
-	os.Setenv("CORS_ALLOWED_ORIGINS", "*")
-	os.Setenv("CORS_ALLOWED_METHODS", "GET POST PUT DELETE OPTIONS")
-	os.Setenv("CORS_ALLOWED_HEADERS", "Accept Authorization Content-Type X-CSRF-Token")
-	os.Setenv("ACCESS_SECRET", "asdfertybvndfghrtyudfghasdfwer")
-	os.Setenv("REFRESH_SECRET", "lhjfghjtyuiycvbnadfrtysdfzcvfg")
-	//os.Setenv("REDIS_DSN", "rediss://:AVNS_BwpVUI_Q4IzmOVS@db-redis-blr1-07067-do-user-10184514-0.b.db.ondigitalocean.com:6379")
-	os.Setenv("REDIS_DSN", "redis://redis:pass@localhost:6379/<db>")
+	//os.Setenv("PORT", "8080")
+	//os.Setenv("DATASOURCES_POSTGRES_HOST", "ec2-18-235-154-252.compute-1.amazonaws.com")
+	//os.Setenv("HEALTHCHECK_TIMEOUT", "30s")
+	//os.Setenv("DATASOURCES_POSTGRES_USER", "obktylemltwdzc")
+	//os.Setenv("DATASOURCES_POSTGRES_PASSWORD", "3492ecaa68258e934a6b71d0fa502f99d45ce6c159a588a403dbdd789aeedf84")
+	//os.Setenv("DATASOURCES_POSTGRES_DATABASE", "ddgndiu11pio51")
+	//os.Setenv("DATASOURCES_POSTGRES_PORT", "5432")
+	//os.Setenv("DATASOURCES_POSTGRES_SCHEMA", "public")
+	//os.Setenv("APP_NAME", "contractor-panel")
+	//os.Setenv("APP_INSTANCE", "instance1")
+	//os.Setenv("LOG_PRETTY_PRINT", "1")
+	//os.Setenv("CORS_ALLOWED_ORIGINS", "*")
+	//os.Setenv("CORS_ALLOWED_METHODS", "GET POST PUT DELETE OPTIONS")
+	//os.Setenv("CORS_ALLOWED_HEADERS", "Accept Authorization Content-Type X-CSRF-Token")
+	//os.Setenv("ACCESS_SECRET", "asdfertybvndfghrtyudfghasdfwer")
+	//os.Setenv("REFRESH_SECRET", "lhjfghjtyuiycvbnadfrtysdfzcvfg")
+	////os.Setenv("REDIS_DSN", "rediss://:AVNS_BwpVUI_Q4IzmOVS@db-redis-blr1-07067-do-user-10184514-0.b.db.ondigitalocean.com:6379")
+	//os.Setenv("REDIS_DSN", "redis://redis:pass@localhost:6379/<db>")
 	cmd.Execute()
 
 }
